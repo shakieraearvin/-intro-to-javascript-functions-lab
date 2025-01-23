934f7a025c597d5ceda98026ceba93595d607b6f
Shakiera Earvin <kieraearvin@gmail.com>
2025-01-23 15:30:12 -0500

    exercise seven done

--- app.js
+++ app.js
@@ -121,3 +121,21 @@ function maxOfThree(num1, num2, num3) {
 
 console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
 
+/*
+Exercise 7: calculateTip()
+
+Create a function called calculateTip. It should take two arguments: 
+the bill amount and the tip percentage (as a whole number). 
+The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
+
+function calculateTip (billAmount, tipPercent) {
+    let Tip = (billAmount * (tipPercent/100));
+    return Tip
+}
+
+console.log('Exercise 7 Result:', calculateTip(50, 20));
